320c800f64423fdb32f4826d233101b0ce389bc8
Joel Chelliah <joel.chelliah@bekk.no>
2016-04-02 22:05:59 +0200

    Homeword 1 finished!

--- homework_1/Hanoi.hs
+++ homework_1/Hanoi.hs
@@ -1,4 +1,16 @@
-module Hanoi(hanoi) where
+module Hanoi(hanoi, hanoi4) where
 
-hanoi :: Int -> String -> String -> String -> [(String, String)]
-hanoi discs p1 p2 p3 = []
+type Peg  = String
+type Move = (Peg, Peg)
+
+hanoi :: Int -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c
+  | n <= 0    = []
+  | otherwise = hanoi (n - 1) a c b ++ (a, b) : hanoi (n - 1) c b a
+
+hanoi4 :: Int -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 n a b c d
+  | n <= 0    = []
+  | n == 1    = [(a,b)]
+  | otherwise = hanoi4 m a c b d ++ hanoi4 (n - m) a b d c ++ hanoi4 m c b a d
+    where m = n `div` 2
