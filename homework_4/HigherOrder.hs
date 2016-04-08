165cf41ce369e7276a89a0dde8471282c7351d79
Joel Chelliah <joel.chelliah@bekk.no>
2016-04-08 20:34:20 +0200

    Homework 4 finished!

--- homework_4/HigherOrder.hs
+++ homework_4/HigherOrder.hs
@@ -12,3 +12,44 @@ fun2' = sum . filter even . takeWhile (/= 1) . iterate f
 
 
 ------------------- Exercise 2 -------------------
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+              deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr mkTree Leaf
+   where mkTree x Leaf = Node 0 Leaf x Leaf
+         mkTree x (Node _ left val right)
+          | kids left <= kids right = Node (len left) (mkTree x left) val right
+          | otherwise               = Node (len left) left val (mkTree x right)
+         len Leaf = 1 :: Integer
+         len (Node i _ _ _) = i + 1
+         kids Leaf = 0 :: Integer
+         kids (Node _ left _ right) = 1 + kids left + kids right
+
+
+------------------- Exercise 3 -------------------
+
+xor :: [Bool] -> Bool
+xor = foldl (\a b -> not a && b || a && not b) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr ((:) . f) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl = foldr . flip
+
+
+------------------- Exercise 4 -------------------
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = (2:) $ (+1) . (*2) <$> filter (`notElem` remove) [1..n]
+  where remove = [i + j + 2 * i * j | j <- [1..n], i <- [1..j]]
+
+sieveSundaram' :: Integer -> [Integer]
+sieveSundaram' n = 2 : foldr sieve [] [1..n]
+  where sieve x
+          | x `elem` remove = id
+          | otherwise       = (2 * x + 1 :)
+        remove = [i + j + 2 * i * j | j <- [1..n], i <- [1..j]]
